41b6c9eb6773fb3a58b5c739896a0c615b25e401
Rajat Maurya <rajat.maurya@joshtechnologygroup.com>
2022-10-15 11:57:33 +0530

    Improve code formatting Remove redundent code and unwanted comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.blog-slider').slick({
         slidesToShow: 3,
-        slidesToScroll: 1, 
+        slidesToScroll: 1,
         infinite: false,
         prevArrow: $(".pp2"),
         nextArrow: $(".nn2")
@@ -13,8 +13,8 @@ $(document).ready(function(){
 let navLinks = document.querySelectorAll(".links")
 let currentActiveLink = 0
 
-navLinks.forEach((navLink, idx)=>{
-    navLink.addEventListener("click", ()=>{
+navLinks.forEach((navLink, idx) => {
+    navLink.addEventListener("click", () => {
         // adding event listener to navLink
         navLinks[currentActiveLink].classList.remove("link-clicked")
         navLinks[idx].classList.add("link-clicked")
